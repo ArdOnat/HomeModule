a11a2bef2e70474a026b44522b6d57d52b4a736d
Arda Onat <ardonat97@hotmail.com>
2021-09-12 16:54:18 +0300

    Fix package dependency.

--- Package.swift
+++ Package.swift
@@ -16,14 +16,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ArdOnat/CoreModule.git", .exact("1.1.2")),
-        .package(url: "https://github.com/ArdOnat/ApiClient.git", .exact("1.1.3"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HomeModule",
-            dependencies: ["CoreModule","ApiClient"]),
+            dependencies: ["CoreModule"]),
         .testTarget(
             name: "HomeModuleTests",
             dependencies: ["HomeModule"]),
