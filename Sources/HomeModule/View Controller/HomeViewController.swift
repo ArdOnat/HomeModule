7a4dc9048c571820a6680a660c1fa720c3389c20
Arda Onat <ardonat97@hotmail.com>
2021-10-21 20:49:25 +0300

    Fix table view cell constraints.

--- Sources/HomeModule/View Controller/HomeViewController.swift
+++ Sources/HomeModule/View Controller/HomeViewController.swift
@@ -136,7 +136,7 @@ extension HomeViewController: UISearchBarDelegate {
 extension HomeViewController: UITableViewDelegate {
     
     public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
+        return tableView.bounds.height / 2.2
     }
     
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
